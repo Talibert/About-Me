782e85c4fcaccd75a1e6338d906bd5f2c1c2c72e
unknown <guilhermetaliberti@gmail.com>
2024-01-29 17:47:27 -0300

    update

--- my-app/src/Components/ContactItem.js
+++ my-app/src/Components/ContactItem.js
@@ -5,9 +5,9 @@ const ContactItem =({url, src, alt}) => {
     return(
         <div className="contactblock">
             <div className="contactitem">
-            <a className="contactlink" href={url} target="_blank" rel="noopener noreferrer">
-            <img className="contactimage" src={src} alt={alt}></img>
-            </a>
+                <a className="contactlink" href={url} target="_blank" rel="noopener noreferrer">
+                <img className="contactimage" src={src} alt={alt}></img>
+                </a>
             </div> 
         </div>   
     )
